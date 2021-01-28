ee5797ef7d8622687358faf6e362d238c086c1e4
esennesh <sennesh.e@northeastern.edu>
2021-01-28 16:41:11 -0500

    model.collections: rewrite iid() to take a morphism and a monoid over feedback
    Signed-off-by: esennesh <sennesh.e@northeastern.edu>

--- combinators/model/collections.py
+++ combinators/model/collections.py
@@ -5,21 +5,34 @@ from probtorch import Trace
 
 from .. import lens, tracing
 
-def iid(dom, n, f=None):
-    if not isinstance(dom, lens.LensTy):
-        dom = lens.LensPRO(dom)
-    if f is not None:
-        assert dom == f.dom
-    if n == 0:
-        return lens.Id(dom)
+class TracedCopy:
+    def __init__(self, n):
+        self._n = n
 
-    def traced_copy(*vals):
-        return vals * n, (0., Trace())
-    def copy_feedback(*vals):
-        return vals[len(dom):]
-    cod = functools.reduce(lambda x, y: x @ y, [dom] * n, lens.LensPRO(0))
-    copy = tracing.TracedLensBox('%d-iid' % n, dom, cod, traced_copy,
-                                 copy_feedback)
+    def __call__(self, *vals):
+        return vals * self._n, (0., Trace())
+
+class CopyFeedback:
+    def __init__(self, dom, n, join=lambda x, y: x + y):
+        self._dom = len(dom.upper)
+        self._n = n
+        self._join = join
+
+    def __call__(self, *vals):
+        assert (len(vals) - self._dom) % self._n == 0
+        k = len(vals) // self._n
+
+        chunks = [vals[self._dom + i*k:self._dom + (i+1)*k] for i
+                  in range(self._n)]
+        return functools.reduce(self._join, chunks)
+
+def iid(f, n, join=lambda x, y: x + y):
+    if n == 0:
+        return lens.Id(f.dom)
+
+    cod = functools.reduce(lambda x, y: x @ y, [f.dom] * n, lens.LensPRO(0))
+    copy = tracing.TracedLensBox('%d-iid' % n, f.dom, cod, TracedCopy(n),
+                                 CopyFeedback(f.dom, n, join))
     if f is not None:
         fs = functools.reduce(lambda g, h: g @ h, [f] * n,
                               lens.Id(lens.LensPRO(0)))
