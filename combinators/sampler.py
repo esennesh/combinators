9a68fd9a17651ca2b39f07f1d65d6bc9133b7328
Eli Sennesh <esennesh@ccis.neu.edu>
2021-01-31 21:55:02 -0500

    sampler.ImportanceSampler: refactor to cache in sample, score, forward, update
    Signed-off-by: Eli Sennesh <esennesh@ccis.neu.edu>

--- combinators/sampler.py
+++ combinators/sampler.py
@@ -2,50 +2,80 @@
 
 import torch.nn as nn
 
-from .tracing import NestedTrace
+from .tracing import NestedTrace, TraceDiagram
 from . import utils
 
 class ImportanceSampler(nn.Module):
-    def __init__(self, target, proposal, batch_shape=(1,)):
+    def __init__(self, name, target, proposal, batch_shape=(1,)):
         super().__init__()
         assert callable(target) and hasattr(target, 'update')
         self._batch_shape = batch_shape
+        self._name = name
+
         self.add_module('target', target)
         if isinstance(proposal, nn.Module):
             self.add_module('proposal', proposal)
         else:
             self.proposal = None
 
+        self._cache = utils.TensorialCache(1, self._sample)
+
     @property
     def batch_shape(self):
         return self._batch_shape
+
+    def _expand_args(self, *args, **kwargs):
+        args = tuple(utils.batch_expand(arg, self.batch_shape, True)
+                     if hasattr(arg, 'expand') else arg for arg in args)
+        kwargs = {k: utils.batch_expand(v, self.batch_shape, True)
+                     if hasattr(v, 'expand') else v for k, v in kwargs.items()}
+        return args, kwargs
+
+    def _sample(self, *args, **kwargs):
+        args, kwargs = self._expand_args(*args, **kwargs)
+        q = NestedTrace()
+
+        if self.proposal is not None:
+            self.proposal(q, *args, **kwargs)
+        return self._score(q, *args, **kwargs)
+
+    def _score(self, q, *args, **kwargs):
+        p = NestedTrace(q=q)
+        result = self.target(p, *args, **kwargs)
+
+        log_weight = p.conditioning_factor(self.batch_shape)
+        assert log_weight.shape == self.batch_shape
+
+        return TraceDiagram.BOX(result, log_weight, p, self._name)
 
     def forward(self, *args, **kwargs):
         if args and isinstance(args[-1], dict):
             kwargs = args[-1]
             args = args[:-1]
-        args = tuple(utils.batch_expand(arg, self.batch_shape, True)
-                     if hasattr(arg, 'expand') else arg for arg in args)
-        kwargs = {k: utils.batch_expand(v, self.batch_shape, True)
-                     if hasattr(v, 'expand') else v for k, v in kwargs.items()}
 
-        if self.proposal is not None:
-            q = NestedTrace()
-            _, q = self.proposal(q, *args, **kwargs)
-            p = NestedTrace(q=q)
-        else:
-            p = NestedTrace()
+        result, _, p, _ = self._cache(*args, **kwargs).box()
+        if result is None:
+            args, kwargs = self._expand_args(*args, **kwargs)
+            self._cache[0] = self._score(p, args, kwargs)
+            result = self.trace.box()[0]
 
-        result, p = self.target(p, *args, **kwargs)
+        return result
 
-        log_weight = p.conditioning_factor(self.batch_shape)
-        assert log_weight.shape == self.batch_shape
-        return result, (log_weight, p)
+    @property
+    def trace(self):
+        if self._cache:
+            return self._cache[0][-1]
+        return None
+
+    def clear(self):
+        self._cache.clear()
 
     def update(self, *args, **kwargs):
         if hasattr(self.proposal, 'update'):
-            self.proposal.update(*args, **kwargs)
-        return self.target.update(*args, **kwargs)
+            self.proposal.update(self.trace, *args, **kwargs)
+        result, trace = self.target.update(self.trace, *args, **kwargs)
+        self._cache[0] = (self._cache[0][0], trace)
+        return result
 
 class VariationalSampler(ImportanceSampler):
     def __init__(self, target, proposal, mk_optimizer, batch_shape=(1,)):
