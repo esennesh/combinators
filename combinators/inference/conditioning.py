1a790d3bfe2caa269fd35863be680091d7cc325f
Eli Sennesh <sennesh.e@northeastern.edu>
2022-02-14 17:41:52 -0500

    inference.conditioning.SequentialConditioner: more flexible data format
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- combinators/inference/conditioning.py
+++ combinators/inference/conditioning.py
@@ -5,16 +5,18 @@ import collections
 from .. import lens, sampler
 
 class SequentialConditioner(lens.Functor):
-    def __init__(self, **vals):
+    def __init__(self, **kwargs):
         super().__init__(lambda lob: lob, self._condition)
-        self._boxes = {k: collections.deque(v) for k, v in vals.items()}
+        self._boxes = {k: collections.deque(v) for k, v in kwargs.items()}
 
     def _condition(self, box):
         if box.name in self._boxes:
             assert isinstance(box, sampler.ImportanceBox)
+
+            data = self._boxes[box.name].popleft()
+            if not isinstance(data, dict):
+                data = {'data': data}
+
             return box.__class__(box.name, box.dom, box.cod, box.target,
-                                 box.proposal, data={
-                                    'data': self._boxes[box.name].popleft(),
-                                    **box.data
-                                 })
+                                 box.proposal, data={**data, **box.data})
         return box
