279e8941382a4c2aea87584a494a86a25ee5db31
esennesh <sennesh.e@northeastern.edu>
2022-02-19 18:52:15 -0500

    signal.Signal: signals don't really form a category hahahaha
    Signed-off-by: esennesh <sennesh.e@northeastern.edu>

--- combinators/signal.py
+++ combinators/signal.py
@@ -4,52 +4,45 @@ from abc import abstractmethod
 from functools import reduce, wraps
 import itertools
 
-from discopy import cartesian, cat, messages, monoidal, rigid
+from discopy import cartesian, messages, monoidal, rigid
 
 class Signal(rigid.Box):
     """
     Wraps Python functions that can be sliced into separate wires, each of which
     may have a cached default value, with domain and codomain information.
     """
-    def __init__(self, dom, cod, function):
+    def __init__(self, dom, function, update):
+        assert callable(function)
         self._function = function
-        super().__init__(repr(function), monoidal.PRO(dom), monoidal.PRO(cod))
+        assert callable(update)
+        self._update = update
+
+        super().__init__(repr(function), monoidal.PRO(dom), monoidal.PRO(dom))
 
     def __repr__(self):
-        return "Signal(dom={}, cod={}, function={})".format(
-            self.dom, self.cod, repr(self._function))
+        return "Signal(dom={}, function={}, update={})".format(
+            self.dom, repr(self._function), repr(self._update))
 
     def __str__(self):
         return repr(self)
+
+    @property
+    def update(self):
+        return self._update
 
     def __call__(self, *args, **kwargs):
         return self._function(*args, **kwargs)
 
     @staticmethod
     def id(dom=0):
-        return Signal(dom, dom, cartesian.untuplify)
-
-    def then(self, *others):
-        assert len(others) == 1
-        other = others[0]
-        if not isinstance(other, Signal):
-            raise TypeError(messages.type_err(Signal, other))
-        if len(self.cod) != len(other.dom):
-            raise cat.AxiomError(messages.does_not_compose(self, other))
-        if self._function == cartesian.untuplify:
-            return other
-        if other._function == cartesian.untuplify:
-            return self
-
-        function = lambda vals: other(*cartesian.tuplify(self(*vals)))
-        return Signal(self.dom, other.cod, function)
+        return Signal(dom, cartesian.untuplify, lambda *xs: None)
 
     def tensor(self, *others):
         assert len(others) == 1
         other = others[0]
         if not isinstance(other, Signal):
             raise TypeError(messages.type_err(Signal, other))
-        dom, cod = self.dom @ other.dom, self.cod @ other.cod
+        dom = self.dom @ other.dom
         if dom == self.dom:
             return self
         if dom == other.dom:
@@ -59,7 +52,10 @@ class Signal(rigid.Box):
             vals0 = cartesian.tuplify(self(*vals[:len(self.dom)]))
             vals1 = cartesian.tuplify(other(*vals[len(self.dom):]))
             return cartesian.untuplify(*(vals0 + vals1))
-        return Signal(dom, cod, product)
+        def product_eff(*vals):
+            self.update(*vals[:len(self.dom)])
+            other.update(*vals[len(self.dom):])
+        return Signal(dom, product, product_eff)
 
     def __getitem__(self, key):
         if isinstance(key, int):
@@ -67,7 +63,11 @@ class Signal(rigid.Box):
                 args = [None for _ in range(len(self.dom))]
                 args[key] = val
                 return self(*args)[key]
-            return Signal(self.dom[key], self.cod[key], index_signal)
+            def index_update(val):
+                args = [None for _ in range(len(self.dom))]
+                args[key] = val
+                self.update(*args)
+            return Signal(self.dom[key], index_signal, index_update)
         if isinstance(key, slice):
             indices = list(itertools.islice(range(len(self.dom)), key.start,
                                             key.stop, key.step))
@@ -76,7 +76,12 @@ class Signal(rigid.Box):
                 for i, v in zip(indices, vals):
                     args[i] = v
                 return self(*args)[key]
-            return Signal(self.dom[key], self.cod[key], slice_signal)
+            def slice_update(*vals):
+                args = [None for _ in range(len(self.dom))]
+                for i, v in zip(indices, vals):
+                    args[i] = v
+                self.update(*args)
+            return Signal(self.dom[key], slice_signal, slice_update)
         raise TypeError(messages.type_err((int, slice), key))
 
     def split(self):
