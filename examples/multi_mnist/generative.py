c6683c62274c68a76b093106b9efccae1fbe2495
Eli Sennesh <sennesh.e@northeastern.edu>
2022-02-22 21:26:20 -0500

    examples.multi_mnist.generative: handle particles, batches, recons separately
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- examples/multi_mnist/generative.py
+++ examples/multi_mnist/generative.py
@@ -4,73 +4,67 @@ import torch
 import torch.nn as nn
 
 class InitialObjectCodes(nn.Module):
-    def __init__(self, what_dim):
+    def __init__(self, what_dim, num_objects):
         super().__init__()
         self._dim = what_dim
+        self._num_objects = num_objects
 
         self.register_buffer('what_loc', torch.zeros(what_dim))
         self.register_buffer('what_scale', torch.ones(what_dim))
 
-    def forward(self, p, batch_shape=(1, 1, 1)):
-        what_locs = self.what_loc.expand(*batch_shape, self._dim)
-        what_scales = self.what_scale.expand(*batch_shape, self._dim)
+    @property
+    def num_objects(self):
+        return self._num_objects
+
+    def forward(self, p, batch_shape=(1,), particle_shape=(1,)):
+        what_locs = self.what_loc.expand(*particle_shape, *batch_shape,
+                                         self._num_objects, self._dim)
+        what_scales = self.what_scale.expand(*particle_shape, *batch_shape,
+                                             self._num_objects, self._dim)
 
         return p.normal(what_locs, what_scales, name='z^{what}')
 
 class InitialObjectLocations(nn.Module):
-    def __init__(self, where_dim):
+    def __init__(self, where_dim, num_objects):
         super().__init__()
         self._dim = where_dim
+        self._num_objects = num_objects
 
         self.register_buffer('where_0_loc', torch.zeros(where_dim))
         self.register_buffer('where_0_scale', torch.ones(where_dim))
 
-    def forward(self, p, batch_shape=(1, 1, 1)):
-        where_locs = self.where_0_loc.expand(*batch_shape, self._dim)
-        where_scales = self.where_0_scale.expand(*batch_shape, self._dim)
+    @property
+    def num_objects(self):
+        return self._num_objects
+
+    def forward(self, p, batch_shape=(1,), particle_shape=(1,)):
+        where_locs = self.where_0_loc.expand(*particle_shape, *batch_shape,
+                                             self._num_objects, self._dim)
+        where_scales = self.where_0_scale.expand(*particle_shape, *batch_shape,
+                                                 self._num_objects, self._dim)
 
         return p.normal(where_locs, where_scales, name='z^{where}')
 
 class StepObjects(nn.Module):
-    def __init__(self, hidden_dim, where_dim, what_dim, spatial_transform):
+    def __init__(self, where_dim, spatial_transform):
         super().__init__()
 
         self.add_module('spatial_transformer', spatial_transform)
 
-        self.obj_avg = nn.Sequential(
-            nn.Linear(what_dim, hidden_dim // 2), nn.ReLU(),
-            nn.Linear(hidden_dim // 2, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, self.spatial_transformer.glimpse_side ** 2)
-        )
-
         self.register_buffer('where_t_scale', torch.ones(where_dim) * 0.2)
-
-    def predict_obj_mean(self, whats):
-        B, P, K, _ = whats.shape
-        obj_avgs = self.obj_avg(whats).view(
-            B, P, K, self.spatial_transformer.glimpse_side,
-            self.spatial_transformer.glimpse_side
-        )
-        return obj_avgs.detach()
 
     def reconstruct(self, obj_avgs, wheres):
         reconstructions = self.spatial_transformer.glimpse2image(obj_avgs,
                                                                  wheres)
-        return torch.clamp(reconstructions.squeeze(dim=2).sum(dim=-3), min=0.0,
-                           max=1.0)
+        return torch.clamp(reconstructions.sum(dim=2), min=0.0, max=1.0)
 
     def forward(self, p, wheres, whats, data=None):
-        B, P, K, _ = whats.shape
-        obj_avgs = self.obj_avg(whats).view(
-            B, P, K, self.spatial_transformer.glimpse_side,
-            self.spatial_transformer.glimpse_side
-        )
-        p.loss(lambda v, t: torch.zeros(B, P, device=obj_avgs.device), obj_avgs,
-               None, name='object_avgs')
-
+        P, B, K, _ = whats.shape
+        obj_avgs = self.spatial_transformer.predict_obj_mean(whats, False)
         wheres_t = p.normal(wheres, self.where_t_scale, name='z^{where}')
 
-        reconstructions = self.reconstruct(obj_avgs, wheres_t)
+        reconstructions = self.spatial_transformer.reconstruct(obj_avgs,
+                                                               wheres_t)
         p.bernoulli(reconstructions, name='x', value=data)
 
         return wheres
