4b60c2c4ef844ddd47d1227c698bc5f629a3b1b8
Eli Sennesh <sennesh.e@northeastern.edu>
2022-02-24 15:34:15 -0500

    inference.metrics: include functions to calculate metrics from diagrams
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- combinators/inference/metrics.py
+++ combinators/inference/metrics.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3
 
+import logging
+import math
 import probtorch
 import torch
 import torch.nn.functional as F
+from discopy import wiring
 
-from .. import utils
+from .. import sampler, utils
+
+def diagram_ess(diagram):
+    _, log_weight = sampler.trace(diagram)
+    result = effective_sample_size(log_weight, log_weight.shape[:-1])
+    logging.info('Effective sample size (ESS) of %d based upon K=%d samples',
+                 result, math.prod(log_weight.shape[:-1]))
+    return result
+
+def diagram_log_Z_hat(diagram):
+    _, log_weight = sampler.trace(diagram)
+    result = log_Z_hat(log_weight, log_weight.shape[:-1])
+    logging.info('Estimated log evidence (log Z) %.8e based upon K=%d samples',
+                 result, math.prod(log_weight.shape[:-1]))
+    return result
 
 def effective_sample_size(log_weights, particle_shape):
     log_normalized = utils.normalize_weights(log_weights, particle_shape, True)
