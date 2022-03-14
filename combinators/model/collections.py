260bab170cd4aae70030e829b6c01888e7c0992e
Eli Sennesh <sennesh.e@northeastern.edu>
2022-03-14 13:18:55 -0400

    model.collections: add sequential_ssm() that doesn't include copy at last step
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- combinators/model/collections.py
+++ combinators/model/collections.py
@@ -20,3 +20,8 @@ def parameterized_ssm(params, state, f):
     assert f.dom == state @ params
 
     return lens.Id(state) @ sampler.Copy(params) >> (f @ lens.Id(params))
+
+def sequential_ssm(params, state, f, n):
+    step = parameterized_ssm(params, state, f)
+    head = sequential(step, n - 1)
+    return head >> f
